75ff4db1e932b3b23acc1b9c9b4cc3ba76a832f6
Yan Carlos Morera <yan.morera@finanzauto.com.co>
2024-08-27 13:14:48 -0500

    feature (CQRS): -add ICommand and ICommandHandler to segregate command type features -add IQuery and IQueryHandler to segregate query type features

---
+++ TasksManagement/Common/CQRS/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TasksManagement.Common.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+
+}
