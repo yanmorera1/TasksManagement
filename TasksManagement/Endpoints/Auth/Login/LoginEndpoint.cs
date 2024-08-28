803b9b759e6ea1b0242972a9fa05ccc1c393d6bf
Yan Carlos Morera <yan.morera@finanzauto.com.co>
2024-08-28 14:00:43 -0500

    feat (login): -add endpoint -add handler

---
+++ TasksManagement/Endpoints/Auth/Login/LoginEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace TasksManagement.API.Endpoints.Auth.Login;
+
+public record LoginRequest(string Email, string Password);
+public record LoginResponse(string AccessToken,
+    long ExpiresIn,
+    DateTime ExpirationDate,
+    string RefreshToken);
+
+public class LoginEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/login", async ([FromBody] LoginRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<LoginCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<LoginResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("Login")
+        .Produces<LoginResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Login")
+        .WithDescription("Login");
+    }
+}
---
+++ TasksManagement/Endpoints/Auth/Login/LoginHandler.cs
@@ -0,0 +1,20 @@
+﻿using TasksManagement.API.Contracts;
+using TasksManagement.API.Services;
+
+namespace TasksManagement.API.Endpoints.Auth.Login;
+
+public record LoginCommand(string Email, string Password) : ICommand<LoginResult>;
+public record LoginResult(string AccessToken,
+    long ExpiresIn,
+    DateTime ExpirationDate,
+    string RefreshToken);
+
+internal class LoginCommandHandler(IAuthService authService) : ICommandHandler<LoginCommand, LoginResult>
+{
+    public async Task<LoginResult> Handle(LoginCommand command, CancellationToken cancellationToken)
+    {
+        var authRequest = new AuthRequest(command.Email, command.Password);
+        var authResponse = await authService.Login(authRequest);
+        return authResponse.Adapt<LoginResult>();
+    }
+}
