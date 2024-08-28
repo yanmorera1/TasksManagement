70e4b1b60aa3c9e7cda8b73b4f8f5c0a6b2ea048
Yan Carlos Morera <yan.morera@finanzauto.com.co>
2024-08-28 14:01:02 -0500

    feat (test): -add endpoint -add handler

---
+++ TasksManagement/Endpoints/Test/TestEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace TasksManagement.API.Endpoints.Test;
+
+public record TestRequest(Guid To, string Message);
+
+public class TestEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/test", async ([FromBody] TestRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<TestCommand>();
+            var result = await sender.Send(command);
+            return Results.Ok();
+        })
+        .WithName("Test")
+        .Produces(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Test")
+        .WithDescription("Test");
+    }
+}
