192c622740804a984070dba04a4bdf8e17d89285
Yan Carlos Morera <yan.morera@finanzauto.com.co>
2024-08-28 14:00:54 -0500

    feat (register): -add endpoint -add handler

---
+++ TasksManagement/Endpoints/Auth/Register/RegisterEndpoint.cs
@@ -0,0 +1,32 @@
+﻿namespace TasksManagement.API.Endpoints.Auth.Register;
+
+public record RegisterRequest(
+    string Names,
+    string LastNames,
+    string Email,
+    string Password,
+    Guid RoleId);
+public record RegisterResponse(
+    string AccessToken,
+    long ExpiresIn,
+    DateTime ExpirationDate,
+    string RefreshToken);
+
+public class RegisterEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/register", async ([FromBody] RegisterRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<RegisterCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<RegisterResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("Register")
+        .Produces<RegisterResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Register")
+        .WithDescription("Register");
+    }
+}
